bcb81afcbf6d261cc396b5a9d7f1bbd5c73cf9cc
Martin Ulleberg <martin.ulleberg@gmail.com>
2022-05-23 19:53:31 +0200

    Use correct method

--- app/jobs/developers/find_stale_developers_job.rb
+++ app/jobs/developers/find_stale_developers_job.rb
@@ -2,7 +2,7 @@ class Developers::FindStaleDevelopersJob < ApplicationJob
   queue_as :default
 
   def perform
-    stale_developers = StaleDevelopersQuery.stale
+    stale_developers = StaleDevelopersQuery.stale_and_not_notified
     stale_developers.each(&:notify_as_stale)
   end
 end
